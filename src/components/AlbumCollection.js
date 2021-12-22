4b238627c6d8ac3749622ba8075f30fed2740b45
jasonemarkson <jasonmarkson76@gmail.com>
2021-12-22 18:28:07 -0500

    finally able to render a list of unique artists properly for the drop down menu :

--- src/components/AlbumCollection.js
+++ src/components/AlbumCollection.js
@@ -3,9 +3,9 @@ import Album from './Album'
 // import Brewery from './BreweryCollection'
 
 function AlbumCollection({albums}) {
-
+    
     return (
-        <div>
+        <div id="album-collection">
             {/* <Brewery /> */}
             {albums.map(album => <Album key={album.id} details={album} />)}
         </div>
--- src/components/AlbumForm.js
+++ src/components/AlbumForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import Artists from './Artists'
 
 function AlbumForm({albums}) {
     const [newAlbum, setNewAlbum] = useState({ title: "", artist_id: "", year: "", album_cover: "" })
 
     const handleSubmit = (event) => {
+        debugger
         event.preventDefault()
         // add fetch POST request
         fetch("http://localhost:3000/albums", {
@@ -13,7 +15,8 @@ function AlbumForm({albums}) {
             }, 
             body: JSON.stringify(newAlbum)
         })
-        event.reset()
+
+        event.target.reset()
     }
     
     const handleChange = (event) => {
@@ -31,11 +34,12 @@ function AlbumForm({albums}) {
             <form id="new-album-form" onSubmit={handleSubmit}>
                 Title: <input id="title" type="text" onChange={handleChange} value={newAlbum.title} />
                 Year: <input id="year" type="text" onChange={handleChange} value={newAlbum.year} />
-                Album Cover URL: <input id="album-cover-url" type="text"  value={newAlbum.album_cover} />
+                Album Cover URL: <input id="album_cover" type="text" onChange={handleChange} value={newAlbum.album_cover} />
                 <p></p>
                 {/* Create an Artist component where I can render all of the unique artists here as part of the New album form */}
-                {/* Artist: 
-                <select id="artist_id" name="artist" onChange={handleChange} value={newAlbum.artist} >   
+                Artist: 
+                <Artists props={albums} />
+                {/* <select id="artist_id" name="artist" onChange={handleChange} value={newAlbum.artist} >   
                     <option>-Select-</option>
                     <option>-New Artist-</option>
                         {
